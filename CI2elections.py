2da67237d6d4eeb49136e9868af271a72e8ce75b
hpeura <hpeura@mail.umw.edu>
2023-11-16 14:02:12 -0500

    Added party voting, removed should_vote() method. For now, everyone votes according to their voting algoritm

--- CI2elections.py
+++ CI2elections.py
@@ -17,6 +17,7 @@ import numpy as np
 import pandas as pd
 import math
 import sys
+import random
 from sklearn.preprocessing import StandardScaler
 from mesa.time import RandomActivation
 from mesa.batchrunner import batch_run
@@ -61,6 +62,20 @@ def party_vote(self, voter):
     for candidate in self.candidates:
         if voter.party == candidate.party:
             return candidate
+
+# Function used at initialization to determine all voters' voting algorithms
+# based on percent_rational. Sets self.voting_algorithm to either "rational"
+# or "party" for every voter.
+def determine_voting_algorithms(self):
+    num_rational = int(percent_rational * N)
+    rational_voters = random.sample(self.schedule.agents, num_rational)
+    for voter in self.schedule.agents:
+        if voter in rational_voters:
+            voter.voting_algorithm = "rational"
+        else:
+            voter.voting_algorithm = "party"
+    
+
     
 # An agent-based model of a society with N voters and num_candidates political
 # candidates for office. Other constructor parameters:
@@ -73,7 +88,7 @@ def party_vote(self, voter):
 #   candidate in opinion space, it will sit out the election
 class Society(mesa.Model):
     def __init__(self, N, p, max_iter, num_candidates, cluster_threshold,
-        no_vote_threshold):
+        no_vote_threshold, percent_rational):
 
         super().__init__()
         self.N = N
@@ -88,6 +103,7 @@ class Society(mesa.Model):
         self.step_num = 0
         self.max_iter = max_iter
         self.party_centroids = {}
+        self.percent_rational = percent_rational
 
         # Create new random candidates, one for each party, and initialize each
         # party's "centroid" to be not actually its centroid of voters, but its
@@ -100,7 +116,7 @@ class Society(mesa.Model):
         # Create random voters, and assign each one to the party whose centroid
         # (candidate; see above) it is closest to in Euclidean opinion space.
         for i in range(self.N):
-            newVoter = Voter(i, self, list(np.random.uniform(0,1,num_opinions)),[], 0)
+            newVoter = Voter(i, self, list(np.random.uniform(0,1,num_opinions)),[], 0, 0)
             min_distance = 100
             closest_party = -1
             for party in self.party_centroids:
@@ -110,7 +126,7 @@ class Society(mesa.Model):
                     closest_party = party
             newVoter.party = closest_party
             self.schedule.add(newVoter)
-
+        determine_voting_algorithms(self)
         self.datacollector = DataCollector(
             agent_reporters={},
             model_reporters={"election_results": Society.elect})
@@ -146,26 +162,28 @@ class Society(mesa.Model):
     #
     # Returns a list of vote counts, order by candidate number.
     def elect(self):
-        vote_counts = {candidate.unique_id: 0 for candidate in self.candidates}
-        distances = []
-        candidate_buckets = {candidate.unique_id:
-                                        [] for candidate in self.candidates}
-        bucket_count = 0
+        real_vote_counts = {candidate.unique_id: 0 for candidate in self.candidates}
+        rational_vote_counts = {candidate.unique_id: 0 for candidate in self.candidates}
+        # Have all agents vote based on their voting algorithm and store the
+        # vote counts in real_vote_counts
         for voter in self.schedule.agents:
-            if voter.should_vote():
+            if voter.voting_algorithm == "rational":
                 chosen_candidate = rational_vote(self, voter)
-                vote_counts[chosen_candidate.unique_id] += 1
-                if voter.bucket not in candidate_buckets[chosen_candidate.unique_id]:
-                    candidate_buckets[chosen_candidate.unique_id].append(voter.bucket)
-                    bucket_count += 1
+            else:
+                chosen_candidate = party_vote(self, voter)
+            real_vote_counts[chosen_candidate.unique_id] += 1
+                
+        # Have all agents vote rationally and store the vote counts in rational_vote_counts
+        for voter in self.schedule.agents:
+            chosen_candidate = rational_vote(self, voter)
+            rational_vote_counts[chosen_candidate.unique_id] += 1
 
         # drift after election
         self.recompute_centroids()
         new_opinions = self.drift()
         for candidate in self.candidates:
             candidate.opinions = list(new_opinions[candidate.unique_id])
-            #print(candidate.opinions)
-        return list(vote_counts.values()) #, distances, candidate_buckets
+        return list(real_vote_counts.values()), list(rational_vote_counts.values())
 
     # candidates drift towards the opinions that will get them the most votes
     def drift(self):
@@ -202,19 +220,9 @@ class Society(mesa.Model):
             for t in tuples_to_consider:
                 candidate.opinions = t
                 for voter in self.schedule.agents:
-                    voting_agent = voter.should_vote()
-                    # TODO Issue #9
-                    # if this is a non-voting agent, ensure it will still be non-voting
-                    # with the candidate's new opinions
-                    if voting_agent == False:
-                        distance = math.sqrt(sum((x - y) ** 2 for x, y in zip(voter.opinions, candidate.opinions)))
-                        if distance < no_vote_threshold:
-                            voting_agent = True
-                    # all voting agents vote
-                    if voting_agent:
-                        chosen_candidate = rational_vote(self, voter)
-                        if chosen_candidate == candidate:
-                            vote_counts[t] += 1
+                    chosen_candidate = rational_vote(self, voter)
+                    if chosen_candidate == candidate:
+                        vote_counts[t] += 1
 
             # if the candidate got 0 votes, pick a random opinion array, given it is between 0-1
             if max(vote_counts.values()) == 0:
@@ -294,11 +302,12 @@ class Candidate(mesa.Agent):
 # at each step of the simulation, voter x may influence voter y's
 # opinion on an issue if they agree on a different issue.
 class Voter(mesa.Agent):
-    def __init__(self, unique_id, model, opinions, bucket, party):
+    def __init__(self, unique_id, model, opinions, bucket, party, voting_algorithm):
         super().__init__(unique_id, model)
         self.opinions = opinions
         self.bucket = bucket
         self.party = party
+        self.voting_algorithm = voting_algorithm
     def step(self):
         # Implements the CI2 influence algorithm.
         # retreives x's neighbors and chooses one at random, y.
@@ -369,19 +378,6 @@ class Voter(mesa.Agent):
             self.bucket = bucket
             buckets.append(bucket)
 
-    # Determine whether this agent is close enough to any candidate in opinion
-    # space to make it worth casting a ballot.
-    def should_vote(self):
-        min_distance = 100
-        for candidate in self.model.candidates:
-            distance = math.sqrt(sum((x - y) ** 2 for x, y in zip(self.opinions, candidate.opinions)))
-            if distance < min_distance:
-                min_distance = distance
-        if min_distance > self.model.no_vote_threshold:
-            return False
-        else:
-            return True
-
 
 
 
@@ -412,7 +408,8 @@ no_vote_threshold = pushaway
 num_candidates = 3
 # Steps between each election
 election_steps = 50
-
+# Proportion of voters who will vote rationally
+percent_rational = 0.5
 
 # Returns true if a bucket is "non-trivial", in that it has 3 or more agents
 # This number may need adjusting based on the total number of voters
@@ -438,13 +435,14 @@ if __name__ == "__main__":
         "num_candidates": num_candidates,
         "max_iter": max_iter,  # only needed for plot caption
         "no_vote_threshold": no_vote_threshold,
+        "percent_rational": percent_rational
     }
 
     if num_sims == 1:
         # Single run.
         s = Society(params["N"], params["p"], params["cluster_threshold"],
             params["num_candidates"], params["max_iter"],
-            params["no_vote_threshold"])
+            params["no_vote_threshold"], params["percent_rational"])
         for i in range(max_iter):
             s.step()
         single_results = s.datacollector.get_model_vars_dataframe()
