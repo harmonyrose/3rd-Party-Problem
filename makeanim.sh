3d1c3c26f52ad320f38fb42a65443641f39e7a98
Stephen <stephen@umw.edu>
2024-01-15 12:01:02 -0500

    Add template script to make animation.

---
+++ makeanim.sh
@@ -0,0 +1,19 @@
+#/usr/bin/env bash
+
+convert -delay 55 -loop 0 \
+    cr0_fracRational.png \
+    cr.05_fracRational.png \
+    cr.10_fracRational.png \
+    cr.15_fracRational.png \
+    cr.20_fracRational.png \
+    cr.25_fracRational.png \
+    cr.30_fracRational.png \
+    cr.35_fracRational.png \
+    cr.40_fracRational.png \
+    cr.45_fracRational.png \
+    cr.50_fracRational.png \
+    cr.55_fracRational.png \
+    cr.60_fracRational.png \
+    cr.65_fracRational.png \
+    cr.70_fracRational.png \
+    cr.gif
